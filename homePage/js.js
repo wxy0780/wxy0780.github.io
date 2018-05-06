fcabeacfa43492a0b2c0d65c24e809fcc964ec45
0780wxy <31151586+0780wxy@users.noreply.github.com>
2018-05-05 23:27:22 -0500

    Add files via upload

---
+++ homePage/js.js
@@ -0,0 +1,81 @@
+var allHref = 7;
+
+var HrefN1 = "百度云资源";
+var HrefI1 = "/img/1.png";
+var Href1 = "/pan/pc";
+
+var HrefN2 = "安卓资源";
+var HrefI2 = "/img/17.png";
+var Href2 = "/andro/pc";
+
+var HrefN3 = "滑稽币";
+var HrefI3 = "/img/18.png";
+var Href3 = "/funny/pc";
+
+var HrefN4 = "相册";
+var HrefI4 = "/img/20.png";
+var Href4 = "/pictures/pc";
+
+var HrefN5 = "音乐";
+var HrefI5 = "/img/21.png";
+var Href5 = "/music/pc";
+
+var HrefN6 = "js窗口";
+var HrefI6 = "/img/22.png";
+var Href6 = "/scriptCommand/pc";
+
+var HrefN7 = "设置";
+var HrefI7 = "/img/23.png";
+var Href7 = "/setting/pc";
+
+var HrefN8 = "●●●";
+var HrefI8 = "/img/404.png";
+var Href8 = "/404/pc";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ homePage/jsp2.js
@@ -0,0 +1,136 @@
+var allHrefs = 20;
+
+var HrefNs1 = "Github";
+var HrefIs1 = "/img/3.png";
+var Hrefs1 = "https://github.com/";
+
+var HrefNs2 = "百度";
+var HrefIs2 = "/img/4.png";
+var Hrefs2 = "https://wap.baidu.com/";
+
+var HrefNs3 = "网易邮箱";
+var HrefIs3 = "/img/5.png";
+var Hrefs3 = "https://mail.163.com/";
+
+var HrefNs4 = "bilibili";
+var HrefIs4 = "/img/6.png";
+var Hrefs4 = "https://www.bilibili.com/";
+
+var HrefNs5 = "网易云音乐";
+var HrefIs5 = "/img/7.png";
+var Hrefs5 = "https://music.163.com/";
+
+var HrefNs6 = "W3school";
+var HrefIs6 = "/img/8.png";
+var Hrefs6 = "http://www.w3school.com.cn/";
+
+var HrefNs7 = "Google";
+var HrefIs7 = "/img/9.png";
+var Hrefs7 = "https://google.com/";
+
+var HrefNs8 = "Youtube";
+var HrefIs8 = "/img/10.png";
+var Hrefs8 = "https://youtube.com/";
+
+var HrefNs9 = "Twitter";
+var HrefIs9 = "/img/11.png";
+var Hrefs9 = "https://twitter.com/";
+
+var HrefNs10 = "iCloud";
+var HrefIs10 = "/img/12.png";
+var Hrefs10 = "https://www.icloud.com/";
+
+var HrefNs11 = "4399";
+var HrefIs11 = "/img/13.png";
+var Hrefs11 = "http://www.4399.com/";
+
+var HrefNs12 = "QQ邮箱";
+var HrefIs12 = "/img/14.png";
+var Hrefs12 = "https://mail.qq.com/";
+
+var HrefNs13 = "百度翻译";
+var HrefIs13 = "/img/15.png";
+var Hrefs13 = "https://fanyi.baidu.com/";
+
+var HrefNs14 = "百度云盘";
+var HrefIs14 = "/img/16.png";
+var Hrefs14 = "https://pan.baidu.com/";
+
+var HrefNs15 = "百度图片";
+var HrefIs15 = "/img/4.png";
+var Hrefs15 = "https://image.baidu.com/";
+
+var HrefNs16 = "百度贴吧";
+var HrefIs16 = "/img/4.png";
+var Hrefs16 = "https://tieba.baidu.com/";
+
+var HrefNs17 = "百度地图";
+var HrefIs17 = "/img/4.png";
+var Hrefs17 = "https://map.baidu.com/";
+
+var HrefNs18 = "";
+var HrefIs18 = "/img/20.png";
+var Hrefs18 = "https://.com/";
+
+var HrefNs19 = "";
+var HrefIs19 = "/img/21.png";
+var Hrefs19 = "https://.com/";
+
+var HrefNs20 = "";
+var HrefIs20 = "/img/22.png";
+var Hrefs20 = "https://.com/";
+/*
+
+
+var HrefNs = "";
+var HrefIs = "/img/.png";
+var Hrefs = "https://.com/";
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
